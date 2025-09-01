0dee2bf6b0b03d8c4021588f8ea0b090d059deec
Khant-Wai-Lin <6731503060@lamduan.mfu.ac.th>
2025-09-01 08:46:18 +0000

    done

--- src/main/java/th/mfu/boot/User.java
+++ src/main/java/th/mfu/boot/User.java
@@ -1,6 +1,34 @@
 package th.mfu.boot;
 
 public class User {
+    private String username;
+    private String displayname;
+    private String email;
 
-    
+    // Getter and Setter for username
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter and Setter for displayname
+    public String getDisplayname() {
+        return displayname;
+    }
+
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
+    }
+
+    // Getter and Setter for email
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
